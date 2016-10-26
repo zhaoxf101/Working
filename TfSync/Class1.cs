97a9a0eb7480b70626be22f04174a3f025aee10d
Administrator <Administrator@WORKER-PC>
2016-10-26 10:50:04 +0800

    添加项目文件。

---
+++ TfSync/Class1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SVNGetTheLastRes
+{
+    public partial class Form1
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Form1()
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //查找最近更新文件，并将命令返回结果输出至txt文件
+                Execute("svnlook changed D:/subversion/project1 > D:/Subversion/project1/hooks/test.txt");
+
+                //读取生成的文件
+                string strPath = ResumeTxt("D:/Subversion/project1/hooks/test.txt");
+
+                //文件内容处理：按换行符将读取的字符串转换成字符串数组
+                string[] aryPath = strPath.Split('\n');
+
+                //循环更新文件
+                for (int i = 0; i < aryPath.Length; i++)
+                {
+                    //处理掉回车符
+                    aryPath[i].Replace('\r', ' ');
+
+                    //经测试，文件中最后一行是空行，但为了避免遗漏，用非空判断，而不是循环的length-1
+                    if (!aryPath[i].Trim().Equals(""))
+                    {
+                        //根据文件中的数据格式，从第五个字符开始才是文件路径
+                        string strFile = aryPath[i].Trim().Substring(4);
+                        //组织命令并执行，其中D:/是项目所在文件夹，根据自己的情况组织
+                        string strCmd = "svn update D:/" + strFile + " --username *** --password ***";
+                        Execute(strCmd);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        public string ResumeTxt(string path)
+        {
+            string str = string.Empty;
+
+            StreamReader reader = new StreamReader(path, System.Text.Encoding.Default);
+            str = reader.ReadToEnd();
+
+            //再通过查询解析出来的的字符串有没有GB2312的字段，来判断是否是GB2312格式的，如果是，则重新以GB2312的格式解析
+            Regex reGB = new Regex("GB2312", RegexOptions.IgnoreCase);
+            Match mcGB = reGB.Match(str);
+            if (mcGB.Success)
+            {
+                StreamReader reader2 = new StreamReader(path, System.Text.Encoding.GetEncoding("GB2312"));
+                str = reader2.ReadToEnd();
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// 执行DOS命令并返回结果
+        /// </summary>
+        /// <param name="dosCommand">Dos命令语句</param>
+        /// <returns>DOS命令返回值</returns>
+        public string Execute(string dosCommand)
+        {
+            return Execute(dosCommand, 0);
+        }
+
+        /// <summary> 
+        /// 执行DOS命令，返回DOS命令的输出
+        /// </summary> 
+        /// <param name="dosCommand">dos命令</param> 
+        /// <param name="milliseconds">等待命令执行的时间（单位：毫秒），如果设定为0，则无限等待</param> 
+        /// <returns>返回DOS命令的输出</returns> 
+        public static string Execute(string dosCommand, int seconds)
+        {
+            string output = ""; //输出字符串
+            if (dosCommand != null && dosCommand != "")
+            {
+                Process process = new Process();//创建进程对象
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";//设定需要执行的命令
+                startInfo.Arguments = "/C " + dosCommand;//设定参数，其中的“/C”表示执行完命令后马上退出
+                startInfo.UseShellExecute = false;//不使用系统外壳程序启动
+                startInfo.RedirectStandardInput = false;//不重定向输入
+                startInfo.RedirectStandardOutput = true; //重定向输出
+                startInfo.CreateNoWindow = true;//不创建窗口
+                process.StartInfo = startInfo;
+                try
+                {
+                    if (process.Start())//开始进程
+                    {
+                        if (seconds == 0)
+                        {
+                            process.WaitForExit();//这里无限等待进程结束
+                        }
+                        else
+                        {
+                            process.WaitForExit(seconds); //这里等待进程结束，等待时间为指定的毫秒
+                        }
+                        output = process.StandardOutput.ReadToEnd();//读取进程的输出
+                    }
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    if (process != null)
+                        process.Close();
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ TfSync/MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TfSync
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string _tfFileName;
+        string _svnFileName;
+        string _syncPath;
+        string[] _workingPaths;
+
+        object _stringLock = new object();
+
+        int _isBusy = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _tfFileName = @"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\TF.exe";
+            _svnFileName = @"C:\Program Files\SlikSvn\bin\svn.exe";
+
+            _syncPath = @"D:\Work\CITSECOS";
+            _workingPaths = new string[] { @"C:\Users\Administrator\Desktop\新建文件夹", @"C:\Users\Administrator\Desktop\Service\CITSERP" };
+        }
+
+        void SyncToSvn()
+        {
+            // tf update
+            // tf get /overwrite
+            Log("tf update", true, true);
+            Execute(_tfFileName, "get /overwrite");
+
+            // svn commit
+            // svn commit -m "s" D:\Work\CITSECOS
+            Log("svn commit", true, true);
+            Execute(_svnFileName, "commit -m \"s\" " + "\"" + _syncPath + "\"");
+
+            // svn update
+            // svn up C:\Users\Administrator\Desktop\新建文件夹
+            foreach (var path in _workingPaths)
+            {
+                Log("svn update " + path, true, true);
+                Execute(_svnFileName, "up " + "\"" + path + "\"");
+            }
+        }
+
+        void SyncToTf()
+        {
+            // svn update
+            // svn up D:\Work\CITSECOS
+            Log("svn update " + _syncPath, true, true);
+            var updateResult = Execute(_svnFileName, "up " + "\"" + _syncPath + "\"");
+
+            //var updateResult = "";
+            //updateResult =
+            //@"正在升级 'D:\Work\CITSECOS':
+            //D    D:\Work\CITSECOS\test\新建文件夹\新建文本文档.txt
+            //A    D:\Work\CITSECOS\test\新建文件夹\新建文本文档 (2).txt
+            //A    D:\Work\CITSECOS\test\新建文件夹\新建文本文档(Updated File Name).txt
+            //U    D:\Work\CITSECOS\test\新建文本文档.txt
+            //更新到版本 132。";
+            Debug.WriteLine(updateResult);
+
+            var lines = updateResult.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            var fileName = "";
+            foreach (var line in lines)
+            {
+                fileName = GetFileName(line);
+                switch (line[0])
+                {
+                    case 'A':
+                        // tf add D:\Work\CITSECOS\test\新建文件夹
+                        Log("add " + fileName);
+                        Execute(_tfFileName, "add " + "\"" + fileName + "\"");
+                        break;
+                    case 'U':
+                        // TF.exe checkout D:\Work\CITSECOS\test\新建文本文档.txt
+                        Log("checkout " + fileName);
+                        Execute(_tfFileName, "checkout " + "\"" + fileName + "\"");
+                        break;
+                    case 'D':
+                        // TF.exe delete D:\Work\CITSECOS\test\新建文件夹\新建文本文档.txt
+                        Log("delete " + fileName);
+                        Execute(_tfFileName, "delete " + "\"" + fileName + "\"");
+                        break;
+                }
+            }
+
+            // TF.exe checkin /comment:"<<< Committed By TFSync >>>" /notes:"代码审阅者"="zhaoxf" /noprompt
+            //Execute(_tfFileName, "checkin /comment:\" <<< Committed By TFSync >>> \" /notes:\"代码审阅者\"=\"zhaoxf\" /noprompt");
+            Execute(_tfFileName, "checkin /notes:\"代码审阅者\"=\"zhaoxf\" /noprompt");
+
+        }
+
+        string Execute(string fileName, string arguments)
+        {
+            var process = new Process();
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            process.StartInfo = processStartInfo;
+            process.Start();
+
+            var outputStream = process.StandardOutput;
+
+            var builder = new StringBuilder();
+            var resultBuilder = new StringBuilder();
+
+            var timer = new Timer(state =>
+            {
+                var stringBuilder = (StringBuilder)state;
+                lock (_stringLock)
+                {
+                    if (stringBuilder.Length > 0)
+                    {
+                        Log(stringBuilder.ToString());
+                        stringBuilder.Clear();
+                    }
+                }
+
+
+            }, builder, 0, 500);
+
+            var value = 0;
+            while ((value = outputStream.Read()) != -1)
+            {
+                lock (_stringLock)
+                {
+                    builder.Append((char)value);
+                }
+
+                resultBuilder.Append((char)value);
+            }
+
+            lock (_stringLock)
+            {
+                timer.Dispose();
+                Log(builder.ToString());
+            }
+
+            return resultBuilder.ToString();
+        }
+
+        void Log(string message, bool withTimeStamp = false, bool withTitleSeparateLine = false)
+        {
+            var builder = new StringBuilder();
+            builder.AppendFormat("{0}{1}{2}", withTimeStamp ? Environment.NewLine + DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss.fff] ") : "", message,
+                withTitleSeparateLine ? 
+                Environment.NewLine + "========================================================================" + Environment.NewLine :
+                "");
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                TxtResult.AppendText(builder.ToString());
+                TxtResult.ScrollToEnd();
+            }));
+        }
+
+        string GetFileName(string logString)
+        {
+            if (!string.IsNullOrEmpty(logString))
+            {
+                if (char.IsLetter(logString[0]))
+                {
+                    for (int i = 1; i < logString.Length; i++)
+                    {
+                        if (!char.IsWhiteSpace(logString[i]))
+                        {
+                            return logString.Substring(i);
+                        }
+                    }
+                }
+                else
+                {
+                    return "";
+                }
+            }
+
+            return "";
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (Interlocked.CompareExchange(ref _isBusy, 1, 0) == 0)
+            {
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    SyncToSvn();
+
+                    //Execute(@"C:\Users\Administrator\Desktop\ConsoleApplication1.exe", "");
+                    Interlocked.Exchange(ref _isBusy, 0);
+                }, null);
+            }
+
+        }
+
+        private void BtnCommit_Click(object sender, RoutedEventArgs e)
+        {
+            if (Interlocked.CompareExchange(ref _isBusy, 1, 0) == 0)
+            {
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    Debug.WriteLine("starting! " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                    SyncToTf();
+                    Debug.WriteLine("completed! " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+
+                    Interlocked.Exchange(ref _isBusy, 0);
+                }, null);
+            }
+
+        }
+    }
+}
